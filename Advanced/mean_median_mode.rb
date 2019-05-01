1f185409352c972f5cf5ca52c2f22c3b3ee7fca7
Charlotte Goldthwaite <chargold201@gmail.com>
2019-05-01 10:56:31 -0400

    added code to create and return hash

--- Advanced/mean_median_mode.rb
+++ Advanced/mean_median_mode.rb
@@ -1,10 +1,14 @@
 def mean_median_mode(arr)
-    mean = arr.reduce(:+).to_f/arr.length
+    hash = {}
+    mean = arr.reduce(:+)/arr.length.to_f
     if arr.length.odd?
         median = arr.sort[(arr.length - 1)/2]
     else
         median = (arr.sort[arr.length/2] + arr.sort[(arr.length/2) - 1])/2.0
     end
     mode = arr.max_by {|i| arr.count(i)}
-    puts "mean: #{mean} median: #{median} mode: #{mode}"
+    hash[:mean] = mean
+    hash[:median] = median
+    hash[:mode] = mode
+    hash
 end
